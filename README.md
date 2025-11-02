19bd76d2bed06fe665e496d06a1b838f5d8e8284
Yuji Sugiura <y.sugiura.0316@gmail.com>
2025-11-02 12:55:57 +0900

    Fix

--- README.md
+++ README.md
@@ -3,3 +3,32 @@
 > [!CAUTION]
 > This is my personal experimental benchmark playground for `oxfmt`.
 
+## ./bench-oxfmt.sh
+
+```
+# Compare architecture, just print versions
+Benchmark 1: oxfmt_current
+  Time (mean ± σ):      24.6 ms ±   0.3 ms    [User: 19.3 ms, System: 5.0 ms]
+  Range (min … max):    24.2 ms …  25.1 ms    10 runs
+
+Benchmark 2: oxfmt_napi
+  Time (mean ± σ):      47.1 ms ±   0.4 ms    [User: 43.7 ms, System: 6.9 ms]
+  Range (min … max):    46.7 ms …  47.6 ms    10 runs
+
+Summary
+  oxfmt_current ran
+    1.91 ± 0.03 times faster than oxfmt_napi
+
+# Compare performance on formatting outline repo with disabling embedded formatting
+Benchmark 1: oxfmt_current
+  Time (mean ± σ):      67.3 ms ±   1.6 ms    [User: 261.5 ms, System: 62.7 ms]
+  Range (min … max):    65.3 ms …  70.3 ms    10 runs
+
+Benchmark 2: oxfmt_napi
+  Time (mean ± σ):     437.3 ms ±   1.7 ms    [User: 2942.4 ms, System: 84.5 ms]
+  Range (min … max):   434.8 ms … 439.1 ms    10 runs
+
+Summary
+  oxfmt_current ran
+    6.50 ± 0.16 times faster than oxfmt_napi
+```
--- bench-oxfmt.sh
+++ bench-oxfmt.sh
@@ -1,16 +1,33 @@
-oxfmt_format() {
+oxfmt_current() {
   ./node_modules/oxfmt-current/bin/oxfmt "$@"
 }
-export -f oxfmt_format
+export -f oxfmt_current
 
-oxfmt_napi_format() {
+oxfmt_napi() {
   node ./node_modules/oxfmt-napi/dist/cli.js "$@"
 }
-export -f oxfmt_napi_format
+export -f oxfmt_napi
 
+# ---
+
+echo "# Compare architecture, just print versions"
 hyperfine --ignore-failure --warmup 3 --runs 10 \
-  --prepare 'git reset --hard' \
   --shell=bash \
   -n oxfmt_current -n oxfmt_napi \
-  'oxfmt_format outline -c ./bench-oxfmt.json' \
-  'oxfmt_napi_format outline -c ./bench-oxfmt.json'
+  'oxfmt_current -V' \
+  'oxfmt_napi -V'
+echo ""
+
+echo "# Compare performance on formatting outline repo with disabling embedded formatting"
+hyperfine --ignore-failure --warmup 3 --runs 10 \
+  -C 'git reset --hard' \
+  --shell=bash \
+  -n oxfmt_current -n oxfmt_napi \
+  'oxfmt_current ./repos/outline -c ./bench-oxfmt.json' \
+  'oxfmt_napi ./repos/outline -c ./bench-oxfmt.json'
+echo ""
+
+# Latest napi version is significantly slower than current Rust version,
+# - even though embedded formatting is disabled.
+# - even though there is no embedded code.
+# - even though disabling napi feature at build time.
